6ae8580ef35602e14167fe7a6ad82ec7d2073376
Mobin <sadeghi.mobin.1900@outlook.com>
2025-12-18 16:42:07 +0330

    Subproject 2 (solve equation)

--- practice/project.cpp
+++ practice/project.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
-using namespace std;
-
-
-int main(){}
+#include <cmath>
+float DegreeOne(float a, float b){
+    float r;
+    r = -b/a;
+    return r;
+}
+float DegreeTwo(float a, float b, float c){
+    float delta, root, root1, root2;
+    delta = (b*b) - 4*(a*c);
+    if (delta < 0){
+        return 'without root';
+    }
+    else{
+        if(delta == 0){
+            root = -b / (2*a);
+            return root;
+        }
+        else if(delta > 0){
+            root1 = ((-b) + sqrt(delta))/(2*a);
+            root2 = ((-b) - sqrt(delta))/(2*a);
+            return root1, root2; 
+        }
+    }
+}
