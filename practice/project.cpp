5dab6fb69f04f47d0c8ebe51b6e73b1dc6ec5aff
Farbod <farbod.divandari85@gmail.com>
2025-12-18 21:34:34 +0330

    final edits

--- practice/project.cpp
+++ practice/project.cpp
@@ -7,7 +7,7 @@ long long factorial(int x)
         return 1;
     return x * factorial(x - 1);
 }
-long long entekhab(int k, int n)
+long long entekhab(long long k, long long n)
 {
     return factorial(n) / (factorial(k) * factorial(n - k));
 }
@@ -32,6 +32,50 @@ long long KMM(long long x, long long y)
 {
     return abs(x * y) / BMM(x, y);
 }
+    int sum(int arr[], int n) {
+        int s=0;
+        for (int i=0; i<n; i++) {
+            s += arr[i];
+        }
+        return s;
+    }
+    int subtract(int arr[], int n) {
+        int s=arr[0];
+        for (int i=1; i<n; i++) {
+            s -= arr[i];
+        }
+        return s;
+    }
+    int multiply(int arr[], int n) {
+        int p = 1;
+        for (int i=0; i<n; i++) {
+            p *= arr[i];
+        }
+        return p;
+    }
+    float DegreeOne(float a, float b){
+        float r;
+        r = -b/a;
+        return r;
+    }
+   void DegreeTwo(float a, float b, float c){
+        float delta, root, root1, root2;
+        delta = (b*b) - 4*(a*c);
+        if (delta < 0){
+            cout<<"without root"<<endl;
+        }
+        else{
+            if(delta == 0){
+                root = -b / (2*a);
+              cout<< root<<endl;
+            }
+            else if(delta > 0){
+                root1 = ((-b) + sqrt(delta))/(2*a);
+                root2 = ((-b) - sqrt(delta))/(2*a);
+               cout<<root1<<endl<< root2; 
+            }
+        }
+    }
 
 int main()
 {
@@ -73,18 +117,19 @@ int main()
             cout << "Please enter the coefficients of the equation." << endl;
             cin >> a >> b;
             cout << DegreeOne(a, b);
+            break;
         }
-        break;
 
         case 2:
         {
             float a, b, c;
             cout << "Please enter the coefficients of the equation." << endl;
             cin >> a >> b >> c;
-            cout << DegreeTwo(a, b, c);
+         DegreeTwo(a, b, c);
+            break;
+        }
         }
         break;
-        }
     }
     case 3:
     {
@@ -103,69 +148,5 @@ int main()
         break;
     }
     }
-void readArray(int arr[], int &n) {
-    cout << "تعداد اعداد را وارد کنید: ";
-    cin >> n;
-    cout << "اعداد را وارد کنید:" << endl;
-    for (int i=0; i<n; i++) {
-        cin >> arr[i];
-    }
-}
-int sum(int arr[], int n) {
-    int s=0;
-    for (int i=0; i<n; i++) {
-        s += arr[i];
-    }
-    return s;
-}
-int subtract(int arr[], int n) {
-    int s=arr[0];
-    for (int i=1; i<n; i++) {
-        s -= arr[i];
-    }
-    return s;
-}
-int multiply(int arr[], int n) {
-    int p = 1;
-    for (int i=0; i<n; i++) {
-        p *= arr[i];
-    }
-    return p;
-}
-void printResults(int arr[], int n) {
-    cout << "جمع: " << sum(arr, n) << endl;
-    cout << "تفریق: " << subtract(arr, n) << endl;
-    cout << "ضرب: " << multiply(arr, n) << endl;
-}
-
-int main() {
-    int arr[100];
-    int n;
-    readArray(arr, n);
-    printResults(arr, n);
-    return 0;
-}
-#include <cmath>
-float DegreeOne(float a, float b){
-    float r;
-    r = -b/a;
-    return r;
-}
-float DegreeTwo(float a, float b, float c){
-    float delta, root, root1, root2;
-    delta = (b*b) - 4*(a*c);
-    if (delta < 0){
-        return 'without root';
-    }
-    else{
-        if(delta == 0){
-            root = -b / (2*a);
-            return root;
-        }
-        else if(delta > 0){
-            root1 = ((-b) + sqrt(delta))/(2*a);
-            root2 = ((-b) - sqrt(delta))/(2*a);
-            return root1, root2; 
-        }
-    }
-}
+    return 0;}
+    
